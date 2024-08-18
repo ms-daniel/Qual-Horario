8b03b0862ffbc5289148547b284e506e3308109f
Carlos Daniel <ms_danniel@outlook.com>
2024-08-18 18:34:16 -0300

    first files

---
+++ src/main/java/Qual/Horario/QualHorarioApplication.java
@@ -0,0 +1,13 @@
+package Qual.Horario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualHorarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualHorarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Qual/Horario/QualHorarioApplicationTests.java
@@ -0,0 +1,13 @@
+package Qual.Horario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualHorarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
